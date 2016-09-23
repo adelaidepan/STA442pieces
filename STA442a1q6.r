00a165d5fbd646e0f1bfdd2e0f5e45d2d0bcc7f2
adelaidepan <panzhihan2014@hotmail.com>
2016-09-22 20:13:26 -0400

    Create STA442a1q6.r

---
+++ STA442a1q6.r
@@ -0,0 +1,15 @@
+sat = read.table("http://www.utstat.toronto.edu/~brunner/data/legal/openSAT.data.txt")
+
+#calculate the difference
+sat$diff <- sat$VERBAL - sat$MATH
+
+#check the mean of difference
+mean(sat$VERBAL)-mean(sat$MATH)
+
+boxplot(sat$diff)
+qqnorm(sat$diff)
+qqline(sat$diff)
+shapiro.test(sat$diff)
+
+#do a paired t-test
+t.test(sat$VERBAL, sat$MATH, mu=0, paired = T, alternative = "two.sided")
